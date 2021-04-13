46945a886cd252633c9701c5eb6d194e96cc973e
KOKULAN M <49874007+Kokulan1763@users.noreply.github.com>
2021-04-13 15:24:16 +0530

    Add files via upload

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into memberservicePremium values(1,'1feb19','1jan19',2,2000,3000);
+insert into memberservicePremium values(2,'1feb19','1jan19',2,2000,3000);
+
+
+insert into claimtable values(1,100,1000,'Hospitalclaim',2,'claimed',2);
+insert into claimtable values(5,120,10001,'NetClaim',3,'unclaimed',5);
+
+
